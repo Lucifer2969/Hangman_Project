bd292ab41a87e343a71899ab037fcb5b4569847d
Lucifer <raghv.afgji@icloud.com>
2025-01-04 19:52:10 +0530

    Project completed

--- hangman.py
+++ hangman.py
@@ -1,19 +1,90 @@
 import random
+stages = ['''
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+     |
+========
+''','''
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+     |
+========
+''','''
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+     |
+========
+''','''
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+     |
+========
+''','''
+ +---+
+ |   |
+ O   |
+     |
+     |
+     |
+     |
+========
+''','''
+ +---+
+ |   |
+     |
+     |
+     |
+     |
+     |
+========
+''']
 word_list = ["camel","baboon","aardvark"]
 word = random.choice(word_list)
 placeholder = ""
-display = ""
+lives = 5
+game_over = False
 print(word)
 word_length = len(word)
-
+correct_word = []
 for position in range(word_length):
     placeholder += '_'
 print(placeholder)
-while True:
+while not game_over:
     guess = input("Guess a letter: ").lower()
-    for letter in word:
-        if letter == guess:
-            display += letter
-        else:
-            display += '_'
+    display = ""
+    if guess in word:
+        for letter in word:
+            if letter == guess:
+                display += letter
+                correct_word.append(letter)
+            elif letter in correct_word:
+                display += letter
+            else:
+                display += '_'
+    else:
+        display += '_'
+        lives -= 1
     print(display)
+    print(stages[lives])
+    if lives == 0:
+        print("You loose")
+        break
+    if '_' not in display:
+        game_over = True
+        print("You win!")
