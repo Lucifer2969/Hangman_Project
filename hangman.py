d381b2f1bfaaa1e1355cc9a3041306c701a8ca8a
Lucifer <raghv.afgji@icloud.com>
2025-01-06 13:22:45 +0530

    Comments added to the code.

--- hangman.py
+++ hangman.py
@@ -2,20 +2,28 @@ import random
 from Hangman_art import stages, logo
 from Hangman_word import word_list
 
+# Generating random word from the word list.
 word = random.choice(word_list)
 placeholder = ""
 lives = 5
 game_over = False
+
+# Using ASCII art to print logo.
 print(logo)
 print(word)
 word_length = len(word)
 correct_word = []
+
+# Generating Empty spaces based on the word length.
 for position in range(word_length):
     placeholder += '_'
 print(placeholder)
+
+# While loop to check the termination condition and for loop to check correct letter in the word.
 while not game_over:
-    guess = input("Guess a letter: ").lower()
+    guess = input("Guess the correct letter: ").lower()
     display = ""
+    print("*****Lives left is:-",lives,"*****")
     if guess in correct_word:
         print("You have already guessed",guess,"letter.")
     for letter in word:
@@ -26,15 +34,17 @@ while not game_over:
             display += letter
         else:
             display += '_'
-
     print(display)
+
+# Conditional statement to check lives left for the user.
     if guess not in word:
         print("You have guessed wrong letter. Lives will be reduced")
         lives -= 1
-        print("*****Lives left is:-",lives,"*****")
         if lives == 0:
-            print("You loose")
+            print(f"You loose. Correct word is {word}")
             game_over = True
+
+# Conditional statement to check if user has won the game.
     if '_' not in display:
         game_over = True
         print("You win!")
