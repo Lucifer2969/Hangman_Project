5f69dba8f09dcab1955627823a61415e581a2f05
Lucifer <raghv.afgji@icloud.com>
2025-01-06 12:43:01 +0530

    Final changes done

---
+++ Hangman_word.py
@@ -0,0 +1,2 @@
+word_list = ["camel","baboon","aardvark","able","account","acid","act","addition","adjustment","advertisement","\
+             after","agreement","air","amount","amusement"]
--- hangman.py
+++ hangman.py
@@ -1,64 +1,12 @@
 import random
-stages = ['''
- +---+
- |   |
- O   |
-/|\  |
-/ \  |
-     |
-     |
-========
-''','''
- +---+
- |   |
- O   |
-/|\  |
-/    |
-     |
-     |
-========
-''','''
- +---+
- |   |
- O   |
-/|\  |
-     |
-     |
-     |
-========
-''','''
- +---+
- |   |
- O   |
-/|   |
-     |
-     |
-     |
-========
-''','''
- +---+
- |   |
- O   |
-     |
-     |
-     |
-     |
-========
-''','''
- +---+
- |   |
-     |
-     |
-     |
-     |
-     |
-========
-''']
-word_list = ["camel","baboon","aardvark"]
+from Hangman_art import stages, logo
+from Hangman_word import word_list
+
 word = random.choice(word_list)
 placeholder = ""
 lives = 5
 game_over = False
+print(logo)
 print(word)
 word_length = len(word)
 correct_word = []
@@ -68,6 +16,8 @@ print(placeholder)
 while not game_over:
     guess = input("Guess a letter: ").lower()
     display = ""
+    if guess in correct_word:
+        print("You have already guessed",guess,"letter.")
     for letter in word:
         if letter == guess:
             display += letter
@@ -76,9 +26,12 @@ while not game_over:
             display += letter
         else:
             display += '_'
+
     print(display)
     if guess not in word:
+        print("You have guessed wrong letter. Lives will be reduced")
         lives -= 1
+        print("*****Lives left is:-",lives,"*****")
         if lives == 0:
             print("You loose")
             game_over = True
